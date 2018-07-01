b7b38b68853d566dc90b699565af272bbd1d68df
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-07-01 18:44:11 +0100

    Debug intake issues

--- hvplot/intake.py
+++ hvplot/intake.py
@@ -2,11 +2,15 @@ from distutils.version import LooseVersion
 
 from . import patch, _hv
 
+import param
+
 try:
     import intake.plotting # noqa
     patch('intake', extension='bokeh')
 except:
     import intake
+    version = LooseVersion(intake.__version__)
+    raise Exception(intake.__version__)
     if LooseVersion(intake.__version__) <= '0.1.5':
         patch('intake', extension='bokeh')
         patch('intake', 'plot')
